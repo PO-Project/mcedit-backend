b5577474cdff07950d4eded784e2ff29f276b201
simek456 <w.simek0147@gmail.com>
2020-01-05 16:31:21 +0100

    prototypowanie paska menu v1

--- src/MceditBackend.cpp
+++ src/MceditBackend.cpp
@@ -1,15 +1,45 @@
 
-#include "MceditBackend.hpp
+#include "MceditBackend.hpp"
 
 #include <ncurses.h>
+#include <panel.h>
+
+enum menu { bar , file , edit };
 
 namespace backends{
 
     void MceditBackend::operator()(){
+        
+        //zmienne
+        WINDOW* my_windows[3];
+        PANEL* my_panels[3];
+
         initscr();
-        printw("Hello world!\n");
-        refresh();
-        getchar();
+
+        my_windows[bar] = newwin(3,20,0,0);
+        my_windows[file] = newwin(10,10,2,0);
+        my_windows[edit] = newwin(10,10,2,9);
+
+        for(int i = 0; i<3; i++)
+            box(my_windows[i],0,0);
+        
+        my_panels[bar] = new_panel(my_windows[bar]); 
+
+        update_panels();
+        doupdate();
+
+        getch();
+        my_panels[file] = new_panel(my_windows[file]);
+        update_panels();
+        doupdate();
+
+        getch();
+        my_panels[edit] = new_panel(my_windows[edit]);
+        update_panels();
+        doupdate();
+
+        getch();
+
         endwin();
     }
 }
